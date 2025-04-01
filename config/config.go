1f3a808f56dee494c61e0eccbe6e432a07224835
Artem Kornilaev <artemkornilaev1@gmail.com>
2025-04-01 19:08:49 +0400

    update: config

--- config/config.go
+++ config/config.go
@@ -2,62 +2,23 @@ package config
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/caarlos0/env/v11"
 )
 
-type (
-	// Config -.
-	Config struct {
-		App     App
-		HTTP    HTTP
-		Log     Log
-		PG      PG
-		RMQ     RMQ
-		Metrics Metrics
-		Swagger Swagger
-	}
+type Config struct {
+	Env         string        `yaml:"env" env-default:"local"`
+	StoragePath string        `yaml:"storage_path" env-required:"true"`
+	TokenTTL    time.Duration `yaml:"token-ttl" env-required:"true"`
+	GRPC        GRPCConfig    `yaml:"grpc"`
+}
 
-	// App -.
-	App struct {
-		Name    string `env:"APP_NAME,required"`
-		Version string `env:"APP_VERSION,required"`
-	}
+type GRPCConfig struct {
+	Port    int           `yaml:"port"`
+	Timeout time.Duration `yaml:"timeout"`
+}
 
-	// HTTP -.
-	HTTP struct {
-		Port           string `env:"HTTP_PORT,required"`
-		UsePreforkMode bool   `env:"HTTP_USE_PREFORK_MODE" envDefault:"false"`
-	}
-
-	// Log -.
-	Log struct {
-		Level string `env:"LOG_LEVEL,required"`
-	}
-
-	// PG -.
-	PG struct {
-		PoolMax int    `env:"PG_POOL_MAX,required"`
-		URL     string `env:"PG_URL,required"`
-	}
-
-	// RMQ -.
-	RMQ struct {
-		ServerExchange string `env:"RMQ_RPC_SERVER,required"`
-		ClientExchange string `env:"RMQ_RPC_CLIENT,required"`
-		URL            string `env:"RMQ_URL,required"`
-	}
-
-	Metrics struct {
-		Enabled bool `env:"METRICS_ENABLED" envDefault:"true"`
-	}
-
-	Swagger struct {
-		Enabled bool `env:"SWAGGER_ENABLED" envDefault:"false"`
-	}
-)
-
-// NewConfig returns app config.
 func NewConfig() (*Config, error) {
 	cfg := &Config{}
 	if err := env.Parse(cfg); err != nil {
--- go.mod
+++ go.mod
@@ -40,6 +40,7 @@ require (
 	github.com/jackc/pgservicefile v0.0.0-20240606120523-5a60cdf6a761 // indirect
 	github.com/jackc/puddle/v2 v2.2.2 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
+	github.com/keshvan/protos v0.0.0-20250401144503-8d40206578a6 // indirect
 	github.com/klauspost/compress v1.18.0 // indirect
 	github.com/lann/builder v0.0.0-20180802200727-47ae307949d0 // indirect
 	github.com/lann/ps v0.0.0-20150810152359-62de8c46ede0 // indirect
